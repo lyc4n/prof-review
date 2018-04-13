645cdae6fbec21fab875dd66fee59a0a718e40da
Norly <canariasnorly@gmail.com>
2018-04-13 21:34:53 +0800

    Adds professor model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180413132057) do
+ActiveRecord::Schema.define(version: 20180413133244) do
+
+  create_table "professors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "subjects", force: :cascade do |t|
     t.string "code"
