9420468233e4891169b48e5bcc8eb6252106fb85
Norly <canariasnorly@gmail.com>
2018-04-14 21:45:08 +0800

    Adds pagination in professors list

--- app/javascript/professor_list/professor_list.js
+++ app/javascript/professor_list/professor_list.js
@@ -1,16 +1,66 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import ProfessorItem from './professor_item'
+import Pagination from 'react-js-pagination'
+
+class ProfessorsFetcher{
+  constructor(){
+    this.endpoint = window.location.origin+window.location.pathname
+  }
+
+  perform(fetchOptions, beforeSendCallback, successCallback){
+    this.beforeSendCallback =  beforeSendCallback
+    $.ajax({
+      url:        this.endpoint,
+      data:       fetchOptions,
+      dataType:   'json',
+      beforeSend: ((request) => {
+        if(this.beforeSendCallback){this.beforeSendCallback()}
+      }).bind(this),
+      success: successCallback,
+      error:   (response) => {
+        alert("Woops, an error occured.")
+      }
+    })
+  }
+}
 
 class ProfessorList extends Component{
+  constructor(props){
+    super(props)
+    this.state = $.extend({}, props)
+  }
+
+  handlePageChange(pageNumber){
+    let fetchOptions = {page: pageNumber}
+    let fetcher = new ProfessorsFetcher
+    fetcher.perform(fetchOptions, null, function(response){
+      this.setState({professors: response.data, meta: response.meta})
+    }.bind(this))
+  }
+
+  renderPagination(){
+    return(
+      <Pagination
+          prevPageText={'← Previous'}
+          nextPageText={'Next →'}
+          hideFirstLastPages
+          activePage={this.state.meta.currentPage}
+          itemsCountPerPage={this.state.meta.perPage}
+          totalItemsCount={this.state.meta.totalEntries}
+          onChange={this.handlePageChange.bind(this)}
+      />
+    )
+  }
   render(){
     return(
       <div className='professor-list'>
-      {
-        this.props.professors.map((professor)=>{
-          return(<ProfessorItem key={professor.id} {...professor} />)
-        })
-      }
+       {
+         this.state.professors.map((professor)=>{
+           return(<ProfessorItem key={professor.id} {...professor} />)
+         })
+       }
+       {this. renderPagination()}
       </div>
     )
   }
